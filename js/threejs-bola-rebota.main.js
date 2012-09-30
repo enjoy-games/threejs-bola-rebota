262e773ca9e959b3d825ab4cc492bc05317641bd
sergio <sergio@sergiomini>
2012-09-30 13:55:53 +0200

    Solucionado texto al copiar-pegar.

--- js/threejs-bola-rebota.main.js
+++ js/threejs-bola-rebota.main.js
@@ -1,43 +1,43 @@
 /*
                                 ESPAÑOL
 
-  Este archivo es parte de 'html5-bola-rebota'.
+  Este archivo es parte de 'threejs-bola-rebota'.
 
   Copyright 2012 Sergio Lindo - <sergiolindo.empresa@gmail.com>
 
-  'html5-bola-rebota' es software libre: usted puede redistribuirlo y/o
+  'threejs-bola-rebota' es software libre: usted puede redistribuirlo y/o
   modificarlo bajo los términos de la Licencia Pública General GNU publicada
   por la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,
   o (a su elección) cualquier versión posterior.
 
-  'html5-bola-rebota' se distribuye con la esperanza de que sea útil, pero
+  'threejs-bola-rebota' se distribuye con la esperanza de que sea útil, pero
   SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita MERCANTIL o de
   APTITUD PARA UN PROPÓSITO DETERMINADO. Consulte los detalles de la Licencia
   Pública General GNU para obtener una información más detallada.
 
   Debería haber recibido una copia de la Licencia Pública General GNU junto a
-  'html5-bola-rebota'. En caso contrario, consulte
+  'threejs-bola-rebota'. En caso contrario, consulte
   <http://www.gnu.org/licenses/>.
 
 
                                 ENGLISH
 
-  This file is part of 'html5-bola-rebota'.
+  This file is part of 'threejs-bola-rebota'.
 
   Copyright 2012 Sergio Lindo - <sergiolindo.empresa@gmail.com>
 
-  'html5-bola-rebota' is free software: you can redistribute it and/or
+  'threejs-bola-rebota' is free software: you can redistribute it and/or
   modify it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or (at your
   option) any later version.
 
-  'html5-bola-rebota' is distributed in the hope that it will be useful,
+  'threejs-bola-rebota' is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
   Public License for more details.
 
   You should have received a copy of the GNU General Public License along with
-  'html5-bola-rebota'. If not, see <http://www.gnu.org/licenses/>.
+  'threejs-bola-rebota'. If not, see <http://www.gnu.org/licenses/>.
 
 */
 ;
@@ -110,4 +110,4 @@
 	});// FIN $(document).ready.
 
 // Información en consola javascript del navegador.
-console.info("Incluído html5-bola-rebota.main.js");
+console.info("Incluído threejs-bola-rebota.main.js");
